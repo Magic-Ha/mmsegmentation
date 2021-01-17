7129ab963c6ff02fa96627700518196c64ff9fca
8ka <1161642912@qq.com>
2021-01-17 21:36:00 +0800

    max mask 5 selected to aggregation

--- mmseg/models/decode_heads/sep_aspp_head.py
+++ mmseg/models/decode_heads/sep_aspp_head.py
@@ -197,13 +197,15 @@ class ConditionalFilterLayer(nn.Module):
         a = torch.sum(mask * target, 2)
         b = torch.sum(mask * mask, 2) + 0.00001
         # b = torch.sum(mask * mask, 2) + 0.001
-        c = torch.sum(target * target, 2) + 0.00001
+        # c = torch.sum(target * target, 2) + 0.00001
+        c = torch.sum(target * target, 2) 
         # c = torch.sum(target * target, 2) + 0.001
-        d = (2 * a + 1) / (b + c + 1)
+        d = (2 * a + 0.00001) / (b + c + 0.00001)
         # return (1 - d).mean()
         return (1 - d).mean()
 
     def ata_loss(self, filters, target, num_class, batch_size):
+        #
         target_b = target.view(batch_size, -1)
         lb_shown = [torch.unique(target_b[bi], sorted=True).long() for bi in range(0, batch_size)]
         for i in range(0, batch_size):
@@ -236,7 +238,8 @@ class ConditionalFilterLayer(nn.Module):
         return 10*cosdist[~torch.isnan(cosdist)].mean(), dist_matrix
 
     def cfloop(self, filter_conv, feat, mask, x, b, k, h, w, topk, delta_mode=False):
-        
+
+        # cat_result = mask.topk(topk,dim=)
         class_feat = torch.bmm(mask, feat) / (h * w)
         class_feat = class_feat.view(b, k * self.ichn, 1, 1)
         filters = filter_conv(class_feat)
@@ -256,16 +259,32 @@ class ConditionalFilterLayer(nn.Module):
         mask = torch.relu(mask)
         mask = self.mask_conv2(mask)
         # mask = torch.sigmoid(mask)
+
         mask = torch.softmax(mask,dim=1)
+        
         pre_mask = mask
         # mask = torch.sigmoid(self.mask_conv(x))
-        # if gt is not None:
-        #     dice_loss = self.multi_class_dice_loss(mask, gt, num_class)
+        if gt is not None:
+            dice_loss = self.multi_class_dice_loss(mask, gt, num_class)
             # pass
         # b k h w
         b, k, h, w = mask.size()
+        # 挑选5类最大的来做mask
+        cat_value, cat_result = mask.topk(5, dim=1)
+        max_mask = F.one_hot(cat_result.long(), num_class)
+        class_selected_mask = max_mask.sum(dim=1).permute(0,3,1,2).float()
+        mask = torch.mul(class_selected_mask, mask)
+        # print("max 5")
+        # mask = torch.softmax(mask,dim=1)
+        
         mask = mask.view(b, k, -1)#B*C*N
-
+        if True:
+            pass
+            # .squeeze().cpu().numpy().astype(np.uint8)
+            #想到一个加速方法 把batch搞成b*k 相当于把batch搞成类维度 这样可以避免for
+            #但是因为只有uint8 所以应该先只挑出来出现过的类别 然后把类别数排序把绝对值压缩下来
+            #一张图里面也不会出现512/4个类吧 然后换回去的时候只取对角线上的跟batch个数相等的方阵就可以降低维度回去了
+            # void_c,atest2 = cv2.connectedComponents(cat_result_sam.astype(np.uint8),connectivity=8)
         feat = feat.view(b, self.ichn, -1)#B*C*N
         feat = feat.permute(0, 2, 1)#B*N*C
         x = x.view(-1, h, w).unsqueeze(0)
@@ -276,8 +295,8 @@ class ConditionalFilterLayer(nn.Module):
             # filters
             # cosdist, dist_matrix = self.ata_loss(filters+self.mask_conv.weight.repeat([b,1,1,1]).clone().detach(), gt, num_class, b)
             cosdist, dist_matrix = self.ata_loss(filters, gt, num_class, b)
-            # return pred, {'loss_CFlayer': dice_loss, 'loss_cosdist': cosdist,
-            return pred, {'loss_cosdist': cosdist,
+            return pred, {'loss_CFlayer': dice_loss, 'loss_cosdist': cosdist,
+            # return pred, {'loss_cosdist': cosdist,
             # return pred, {'loss_cosdist': cosdist,
                         #    'loss_cpcosdist': cpcosdist, 'pre_mask': pre_mask}
                            'pre_mask': pre_mask}
@@ -347,10 +366,10 @@ class CFDSASPPHead(DepthwiseSeparableASPPHead):
             mode='bilinear',
             align_corners=(self.align_corners))
         # loss['max_segl'] = max(seg_label)
-        loss['loss_dice'] = self.cf_layer.multi_class_dice_loss(
-                                                pre_seg_logit,
-                                                seg_label.squeeze(dim=1),
-                                                self.num_classes)
+        # loss['loss_dice'] = self.cf_layer.multi_class_dice_loss(
+        #                                         pre_seg_logit,
+        #                                         seg_label.squeeze(dim=1),
+        #                                         self.num_classes)
         seg_logit = resize(
             input=seg_logit,
             size=(seg_label.shape[2:]),
@@ -374,18 +393,18 @@ class CFDSASPPHead(DepthwiseSeparableASPPHead):
             seg_label,
             weight=seg_weight,
             ignore_index=(self.ignore_index))
-        loss['losg_pre_seg'] = 0.4*self.loss_decode(
-            pre_seg_logit,
-            seg_label,
-            weight=seg_weight,
-            ignore_index=(self.ignore_index))
+        # loss['losg_pre_seg'] = 0.4*self.loss_decode(
+        #     pre_seg_logit,
+        #     seg_label,
+        #     weight=seg_weight,
+        #     ignore_index=(self.ignore_index))
         # loss['coarse_loss_seg'] = self.loss_decode(
         #     coarse_logit,
         #     seg_label,
         #     weight=coarse_seg_weight,
         #     ignore_index=(self.ignore_index))
         loss['acc_seg'] = accuracy(seg_logit, seg_label)
-        loss['acc_pre_seg'] = accuracy(pre_seg_logit, seg_label)
+        # loss['acc_pre_seg'] = accuracy(pre_seg_logit, seg_label)
 
         loss.update(extra_loss)
 
